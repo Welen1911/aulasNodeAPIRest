c57883d4956aceb329d8cfaacaa3c9941d9f3e20
Welen1911 <welenalmeida2210@gmail.com>
2023-06-12 10:33:15 -0300

    yup implementation

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -1,12 +1,32 @@
 import { Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
+import * as yup from "yup";
 interface ICidade {
-    nome : string;
-    estado : string;
+    nome: string;
+    estado: string;
 }
-export const Create = (req: Request<{},{},ICidade>, res: Response) => {
-    const data = req.body;
+
+const bodyValidation: yup.Schema<ICidade> = yup.object().shape({
+    nome: yup.string().required().min(3),
+    estado: yup.string().required().min(2)
+})
+
+export const Create = async (req: Request<{},{},ICidade>, res: Response) => {
+    
+    let data: ICidade | undefined = undefined;
+
+    try {
+        data = await bodyValidation.validate(req.body); 
+    } catch (error) {
+        const yupError = error as yup.ValidationError;
+        return res.json({
+            errors: {
+                default: yupError.message
+            }
+        })
+    }
    
+    res.send(data);
     
-    
-    return res.send(`Cidade: ${data.nome};</br>Estado: ${data.estado};`);
+
 }
